06af26d6ca97e1f9bd18d3a09e37664b67059a18
cmgn <conormcgovern25@gmail.com>
2018-05-24 14:21:38 +0100

    fixed main

--- euler/haskell/Problem12.hs
+++ euler/haskell/Problem12.hs
@@ -6,6 +6,9 @@ getDivisors n = [(x, div n x) | x <- [1..(ceiling . sqrt $ fromIntegral n)], mod
 divisorCount :: Integer -> Int
 divisorCount = length . nub . foldl (++) [] . map (\(a, b) -> [a, b]) . getDivisors
 
--- solution :: Integer
+solution :: Integer
 solution = head $ dropWhile ((< 500) . divisorCount) triangular
   where triangular = scanl1 (+) [1..]
+
+main :: IO ()
+main = print $ solution
